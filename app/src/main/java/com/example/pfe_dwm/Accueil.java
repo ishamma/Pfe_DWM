feba7a1c765a26ea9fc745ef17aa1bbdba754dab
ishamma <46860784+ishamma@users.noreply.github.com>
2021-08-03 23:01:18 +0100

    commit

--- app/src/main/java/com/example/pfe_dwm/Accueil.java
+++ app/src/main/java/com/example/pfe_dwm/Accueil.java
@@ -13,9 +13,9 @@ import android.view.Menu;
         setContentView(R.layout.activity_accueil);
     }
 
-     @Override
-     public boolean onCreateOptionsMenu(Menu menu) {
-         getMenuInflater().inflate(R.menu.menu_patient,menu);
-         return true;
-     }
+//     @Override
+//     public boolean onCreateOptionsMenu(Menu menu) {
+//         getMenuInflater().inflate(R.menu.menu_patient,menu);
+//         return true;
+//     }
  }
