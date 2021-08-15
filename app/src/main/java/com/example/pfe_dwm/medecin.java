97bbe223fc9bc73f8d0833cd8466b254b2ff76c2
kurunuma <nouhailaelarroum99@gmail.com>
2021-08-15 23:54:24 +0100

    puck u

---
+++ app/src/main/java/com/example/pfe_dwm/medecin.java
@@ -0,0 +1,14 @@
+package com.example.pfe_dwm;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class medecin extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.medecin_acceuil);
+    }
+}
---
+++ app/src/main/java/com/example/pfe_dwm/secretaire.java
@@ -0,0 +1,14 @@
+package com.example.pfe_dwm;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class secretaire extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.secretaire_acceuil);
+    }
+}
