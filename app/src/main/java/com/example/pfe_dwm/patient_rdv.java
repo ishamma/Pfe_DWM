51cc223be9c1fe4bc3bae8677e0be0042b51cdc3
ishamma <46860784+ishamma@users.noreply.github.com>
2021-08-28 00:47:39 +0100

    commit

--- app/src/main/java/com/example/pfe_dwm/patient_rdv.java
+++ app/src/main/java/com/example/pfe_dwm/patient_rdv.java
@@ -5,12 +5,13 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
-import android.widget.Button;
 import android.widget.CalendarView;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.material.navigation.NavigationView;
 
@@ -20,12 +21,14 @@ import org.json.JSONObject;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Set;
 
 public class patient_rdv extends AppCompatActivity {
     private TextView name, date;
     private CalendarView calendrier;
     private RadioGroup time,timeap;
-    private Button btn;
+    ArrayList<String> ih;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -35,46 +38,106 @@ public class patient_rdv extends AppCompatActivity {
         date = findViewById(R.id.maDate);
         time = findViewById(R.id.radioC);
         timeap = findViewById(R.id.radioap);
-        btn = findViewById(R.id.reserver);
         name.setText(Session.user_name);
         calendrier.setMinDate(System.currentTimeMillis() - 1000);
-        ArrayList<String> ih = new ArrayList<>();
-        ArrayList<String> nouha = new ArrayList<>();
-        ih.add("9:00");
-        ih.add("10:00");
-        ih.add("11:00");
-        ih.add("12:00");
-        nouha.add("14:00");
-        nouha.add("15:00");
-        nouha.add("16:00");
-        nouha.add("17:00");
+
+//        ArrayList<String> nouha = new ArrayList<>();
+//        ih.add("9:00");
+//        ih.add("10:00");
+//        ih.add("11:00");
+//        ih.add("12:00");
+//        ih.add("14:00");
+//        ih.add("15:00");
+//        ih.add("16:00");
+//        ih.add("17:00");
 
 
-        for ( String heure: ih ) {
-            RadioButton rb = new RadioButton(this);
-            rb.setText(heure);
-            time.addView(rb);
-        }
-        for ( String heureap: nouha   ) {
-            RadioButton rb = new RadioButton(this);
-            rb.setText(heureap);
-            timeap.addView(rb);
-        }
+//        for ( String heure: ih ) {
+//            RadioButton rb = new RadioButton(this);
+//            rb.setText(heure);
+//            time.addView(rb);
+//        }
+//        for ( String heureap: nouha   ) {
+//            RadioButton rb = new RadioButton(this);
+//            rb.setText(heureap);
+//            timeap.addView(rb);
+//        }
 
         calendrier.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
             @Override
             public void onSelectedDayChange(@NonNull CalendarView view, int year, int month, int dayOfMonth) {
+                //ih.clear();
+//                Log.i("IH list",ih.toString());
+
+                ih = new ArrayList<>();
                 String Ndate = String.valueOf(dayOfMonth)+"/"+String.valueOf(month)+"/"+String.valueOf(year);
                 date.setText(Ndate);
+
+                String sql = "SELECT  * FROM `creneaux` ";
+                HashMap<String, String> params = new HashMap<String, String>();
+                params.put("sql", sql);
+
+                PerformNetworkRequest request = new PerformNetworkRequest(Api.query, params, new PerformNetworkRequest.AsyncResponse() {
+                    @Override
+                    public void processFinish(JSONArray output){
+                        for(int i =0 ; i<output.length();i++){
+                            try {
+                                ih.add(output.getJSONObject(i).getString("heure"));
+                            } catch (JSONException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                        Log.i("IH list",ih.toString());
+                        fill(ih);
+
+                    }
+                });
+                request.execute();
+
+
+
+
+
             }
         });
-        btn.setOnClickListener(new View.OnClickListener() {
+
+
+        time.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
             @Override
-            public void onClick(View v) {
-                Intent i = new Intent(patient_rdv.this,Accueil.class);
-                startActivity(i);
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                Log.i("LOG","Entering");
+                RadioButton radioButton = group.findViewById(checkedId);
+                String index = radioButton.getText().toString();
+                Log.i("LOG",index);
+                Toast toast=Toast.makeText(getApplicationContext(),index,Toast.LENGTH_SHORT);
+                toast.setMargin(50,50);
+                toast.show();
+                date.setText(date.getText()+" "+index+""+":00");
+
             }
         });
+
+    }
+
+    public  void fill(ArrayList<String> lst){
+        Log.i("IH list",lst.toString());
+
+        time = findViewById(R.id.radioC);
+        timeap = findViewById(R.id.radioap);
+        for ( int i = 0 ; i < 4 ; i++ ) {
+            RadioButton rb = new RadioButton(getApplicationContext());
+            rb.setText(lst.get(i));
+            rb.setHint("22");
+            time.addView(rb);
+        }
+        for ( int i = 4 ; i <lst.size()-1 ; i++ ) {
+            RadioButton rb = new RadioButton(getApplicationContext());
+            rb.setText(lst.get(i));
+            rb.setHint("22");
+            timeap.addView(rb);
+        }
+
+
     }
 
 }
