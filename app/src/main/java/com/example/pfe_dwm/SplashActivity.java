02387a69dff4be90863f2027323adafb53898972
ishamma <46860784+ishamma@users.noreply.github.com>
2021-09-14 22:58:48 +0100

    uuuuuuuuuuu

---
+++ app/src/main/java/com/example/pfe_dwm/SplashActivity.java
@@ -0,0 +1,45 @@
+package com.example.pfe_dwm;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.graphics.drawable.Animatable;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+import android.view.animation.AccelerateInterpolator;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
+import android.widget.ImageView;
+
+public class SplashActivity extends AppCompatActivity {
+
+    public static int SPLASH_SCREEN_TIMEOUT=2000;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_splash);
+
+
+        Animation fadeOut = new AlphaAnimation(1,0);
+        fadeOut.setInterpolator(new AccelerateInterpolator());
+        fadeOut.setStartOffset(500);
+        fadeOut.setDuration(1800);
+
+        ImageView image = findViewById(R.id.imageView3);
+
+        image.setAnimation(fadeOut);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent  = new Intent(SplashActivity.this,Accueil.class);
+                startActivity(intent);
+                finish();
+            }
+        },SPLASH_SCREEN_TIMEOUT);
+
+    }
+}
